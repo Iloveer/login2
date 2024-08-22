d844ae7b58fb3039752b183191b3c80e5d61fd00
Astro-True <astro09c@gmail.com>
2024-08-22 09:52:49 -0400

    login post

--- login.js
+++ login.js
@@ -1,19 +1,18 @@
 // login.js
 
-$(document).ready(function() {
-    $("#login-form").submit(function(event) {
-        event.preventDefault();  // Evita que el formulario se envíe de manera convencional
-        
+$(document).ready(function () {
+    $("#login-form").submit(function (event) {
+        event.preventDefault();
         $.ajax({
             type: "POST",
-            url: "/login",  // Endpoint del backend que manejará el login
-            data: $(this).serialize(),  // Serializa los datos del formulario
-            success: function(response) {
+            url: "/login",
+            data: $(this).serialize(),
+            success: function (response) {
                 // Maneja la respuesta exitosa del servidor
                 alert("Login exitoso");
                 // Redireccionar o mostrar un mensaje de éxito
             },
-            error: function(error) {
+            error: function (error) {
                 // Maneja los errores
                 alert("Error en el login: " + error.responseText);
             }
