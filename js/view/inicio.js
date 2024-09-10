e2c1848fbea8e91808e9a614b15ebbefc0109ab4
Iloveer <erlindaquispe84@gamil.com>
2024-09-10 16:13:26 -0400

    ordenamiento

--- js/view/inicio.js
+++ js/view/inicio.js
@@ -4,3 +4,35 @@ function renderInicio() {
   const plantilla = '<h1>inicio</h1>';
   container.append(plantilla);
 }
+
+const apiUrl = 'http://192.168.1.12/Usuario';
+
+// Función para cargar los datos en la tabla
+function cargarDatosEnTabla(datos) {
+    const tabla = document.getElementById('datos-tabla')?.getElementsByTagName('tbody')[0];
+    if (!tabla) {
+        console.error('Elemento de tabla no encontrado.');
+        return;
+    }
+    // Ordenar los datos por el campo 'nombre' alfabéticamente
+    datos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+
+    // Vaciar la tabla antes de insertar nuevos datos
+    tabla.innerHTML = '';
+    datos.forEach(dato => {
+        let fila = tabla.insertRow();
+
+        let celdaId = fila.insertCell(0);
+        let celdaNombre = fila.insertCell(1);
+        let celdaCreateAt = fila.insertCell(2);
+        let celdaUpdateAt= fila.insertCell(3);
+        
+
+        celdaId.textContent = dato.id || 'N/A';
+        celdaNombre.textContent = dato.nombre;
+        celdaCreateAt.textContent = new Date (dato.createAt).toLocaleDateString();
+        celdaUpdateAt.textContent =new Date (dato.updateAt).toLocaleDateString();
+        //celdaFechaNacimiento.textContent = new Date(dato.fechanacimiento).toLocaleDateString();
+        
+    });
+}
